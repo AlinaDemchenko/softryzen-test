352577b185bb8dba6e46b143399f5d4ccbe89c6e
Alina Demchenko <demalina88@gmail.com>
2023-08-13 03:44:31 -0700

    desk_hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <section id="hero">
-      <h1>Uncover Carpathian’s Secrets</h1>
+      <h1 className="text-8xl font-thin uppercase tracking-tighter text-red-700"><span className="font-medium">Uncover</span> Carpathian’s Secrets</h1>
     </section>
   );
 }
